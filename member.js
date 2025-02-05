8cce9b7bcd0cfa52d0bde4c2e5acd0cbdd3350c3
Luma <lumamartinsguimaraes@gmail.com>
2025-02-05 12:47:46 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {    
+    var member = {
+        name: 'John Doe',
+        age: 25,
+        skills: ['HTML', 'CSS', 'JS'],
+        address: {
+            city: 'Boston',
+            state: 'MA',
+            country: 'USA'
+        }
+    };
+
+    return member;
+}
